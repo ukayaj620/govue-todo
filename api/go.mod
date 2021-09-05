dddd84c88885e01ea2aee01966c125cd3a94ef16
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-09-06 02:34:30 +0700

    chore: add cors package

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module github.com/ukayaj620/govue-todo/api
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.4
 	github.com/joho/godotenv v1.3.0 // indirect
 	gorm.io/driver/mysql v1.1.2
--- api/routes/router.go
+++ api/routes/router.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"os"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/ukayaj620/govue-todo/api/controllers"
 )
 
 func InitRouter() {
+	result := godotenv.Load()
+
+	if result != nil {
+		panic(result.Error())
+	}
+
 	router := gin.Default()
 
 	todoRouter := router.Group("/todo")
@@ -16,5 +26,7 @@ func InitRouter() {
 		todoRouter.DELETE("/delete/:id", controllers.DeleteTodo)
 	}
 
-	router.Run()
+	router.Use(cors.Default())
+
+	router.Run(os.Getenv("SERVER_PORT"))
 }
