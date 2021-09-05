70fa843c4c136300ec8fbe6c94aad90293b091b1
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-09-06 02:35:07 +0700

    feat: fetch todos data

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,51 @@
 <template>
-  <div>Hello</div>
+  <h1>GoVue Todo List</h1>
+  <div class="todoList" v-for="{ id, title, createdAt } in todos" :key="id">
+    <p>{{ title }}</p>
+    <p>{{ createdAt }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from "axios";
+
+type Todo = {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+};
 
 export default defineComponent({
   name: "App",
+  data() {
+    return {
+      todos: [] as Todo[],
+    };
+  },
+  async mounted() {
+    const { data } = await axios.get("/todo/all");
+    const formattedData = data.map(
+      ({
+        ID,
+        Title,
+        CreatedAt,
+        UpdatedAt,
+      }: {
+        ID: string;
+        Title: string;
+        CreatedAt: string;
+        UpdatedAt: string;
+      }) => ({
+        id: ID,
+        title: Title,
+        createdAt: CreatedAt,
+        updatedAt: UpdatedAt,
+      })
+    ) as Todo[];
+    this.todos = formattedData;
+  },
 });
 </script>
 
