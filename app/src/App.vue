34ea6e2e4bb9c1598a785d84c7d26fa581c77ff1
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-09-07 11:49:44 +0700

    feat: create and update todo feature

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <h1>GoVue Todo List</h1>
-  <div class="todoList" v-for="{ id, title, createdAt } in todos" :key="id">
-    <p>{{ title }}</p>
-    <p>{{ createdAt }}</p>
+  <div id="app">
+    <h1>GoVue Todo List</h1>
+    <form method="POST" class="todo-form" v-on:submit.prevent="createTodo">
+      <input
+        v-model="todo"
+        type="text"
+        name="todo"
+        class="todo-input"
+        placeholder="what to do?"
+      />
+    </form>
+    <div method="POST" class="todos" v-for="{ id, title } in todos" :key="id">
+      <div class="todo">
+        <form v-if="id === editedTodoId" v-on:submit.prevent="updateTodo">
+          <input type="text" name="editedTodoTitle" v-model="editedTodoTitle" />
+        </form>
+        <p v-else>{{ title }}</p>
+        <button @click="cancel" v-if="id === editedTodoId">Cancel</button>
+        <button @click="updateTodo" v-if="id === editedTodoId">Confirm</button>
+        <button @click="editTodo(id, title)" v-else>Edit</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,29 +40,87 @@ export default defineComponent({
   data() {
     return {
       todos: [] as Todo[],
+      todo: "",
+      editedTodoId: "",
+      editedTodoTitle: "",
     };
   },
-  async mounted() {
-    const { data } = await axios.get("/todo/all");
-    const formattedData = data.map(
-      ({
-        ID,
-        Title,
-        CreatedAt,
-        UpdatedAt,
-      }: {
-        ID: string;
-        Title: string;
-        CreatedAt: string;
-        UpdatedAt: string;
-      }) => ({
-        id: ID,
-        title: Title,
-        createdAt: CreatedAt,
-        updatedAt: UpdatedAt,
-      })
-    ) as Todo[];
-    this.todos = formattedData;
+  mounted() {
+    this.fetchTodos();
+  },
+  methods: {
+    async fetchTodos() {
+      const { data } = await axios.get("/todo/all");
+      const formattedData = data.map(
+        ({
+          ID,
+          Title,
+          CreatedAt,
+          UpdatedAt,
+        }: {
+          ID: string;
+          Title: string;
+          CreatedAt: string;
+          UpdatedAt: string;
+        }) => ({
+          id: ID,
+          title: Title,
+          createdAt: CreatedAt,
+          updatedAt: UpdatedAt,
+        })
+      ) as Todo[];
+      this.todos = formattedData;
+    },
+    async createTodo() {
+      try {
+        const payload = {
+          title: this.todo,
+        };
+        const { data } = await axios.post("/todo/create", payload);
+        const formattedData = {
+          id: data?.ID,
+          title: data?.Title,
+          createdAt: data?.CreatedAt,
+          updatedAt: data?.UpdatedAt,
+        } as Todo;
+        this.todos.push(formattedData);
+        this.todo = "";
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    editTodo(id: string, title: string) {
+      this.editedTodoId = id;
+      this.editedTodoTitle = title;
+    },
+    cancel() {
+      this.editedTodoId = "";
+      this.editedTodoTitle = "";
+    },
+    async updateTodo() {
+      try {
+        const payload = {
+          id: this.editedTodoId,
+          title: this.editedTodoTitle,
+        };
+        console.log(payload);
+        const { data } = await axios.put(`/todo/update/${payload.id}`, payload);
+        const formattedData = {
+          id: data?.ID,
+          title: data?.Title,
+          createdAt: data?.CreatedAt,
+          updatedAt: data?.UpdatedAt,
+        } as Todo;
+        const updatedTodosIndex = this.todos.findIndex(
+          (todo) => todo.id === formattedData.id
+        );
+        this.todos[updatedTodosIndex] = formattedData;
+        console.log(this.todos);
+        this.cancel();
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 });
 </script>
@@ -57,5 +133,28 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.todo-form {
+  width: 100%;
+}
+
+.todo-input {
+  width: 100%;
+}
+
+.todos {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 512px;
+}
+
+.todo {
+  width: 100%;
+  display: flex;
 }
 </style>
